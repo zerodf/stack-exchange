af4b92e1763400f05de6318766dc79b2de030bf9
Alex <zerodf@users.noreply.github.com>
2018-06-12 12:57:57 -0400

    Update athena_run_download.sh

--- stackoverflow/50806014/athena_run_download.sh
+++ stackoverflow/50806014/athena_run_download.sh
@@ -6,4 +6,4 @@
 # wait for it to finish, and download the results
 
 eid=`aws athena start-query-execution --query-string "select count(*) from test_null_unquoted" --query-execution-context Database=SOMEDATABASE--result-configuration OutputLocation=s3://SOMEBUCKET/ --output text --output text` && until aws athena get-query-execution --query-execution-id=$eid --output text | grep "SUCCEEDE
-D"; do sleep 10 | echo "waiting..."; done && aws s3 cp s3://SOMEBUCKET/$eid.csv . && unset eid
+D"; do sleep 30 | echo "waiting..."; done && aws s3 cp s3://SOMEBUCKET/$eid.csv . && unset eid
